d4d55b2aab751048aab88ef2df12f56632a03d9b
Matt on Elementary VM <mgsullivan.musc@gmail.com>
2015-03-18 15:49:16 -0400

    added friendship action button helper and status detection

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,36 @@ class User < ActiveRecord::Base
   def pending_friend_requests_to
     self.friendships.where(state: "pending")
   end
+  def friendship_status(user_2)
+    friendship = Friendship.where(user_id:[self.id, user_2.id],friend_id:[self.id, user_2.id])
+    unless friendship.any?
+      return "not_friends"
+    else 
+      if friendship.first.state=="active"
+        return "friends"
+      else
+        if friendship.first.user==self
+          return "pending"
+        else
+          return "requested"
+        end
+      end
+    end
+  end  
+
+    # if self.active_friends.include?(user_2)
+    #   return "active"
+    # elsif self.pending_friend_requests_from.include?(user_2)
+    #   return "requested"
+    # elsif self.pending_friend_requests_to.include?(user_2)
+    #   return "pending"
+    # elsif self==user_2
+    #   return "self"
+    # else
+    #   return "not a friend"
+    # end
+  
+        
+
 
 end
