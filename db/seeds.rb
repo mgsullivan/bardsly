cb413298742bf91728b12a3b8a916cefd1973c95
Matthew Sullivan <mgsullivan.musc@gmail.com>
2015-03-17 20:48:53 -0400

    added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+	username: "test1" , 
+	email: "test1@test.com",
+	age: 44 ,
+	gender: 'password', 
+	password: 'password',
+	password_confirmation: 'password', 
+	bio: 'Auto-generated user. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar luctus diam, sit amet pulvinar lacus tincidunt vitae. Duis porttitor velit leo, id interdum eros aliquam vel. Aenean finibus imperdiet blandit. Praesent condimentum ante nibh, in vestibulum mauris dapibus sit amet. Mauris tincidunt non tortor eu pulvinar. Morbi lacinia lobortis nibh sed accumsan. Suspendisse ac metus tincidunt metus congue luctus quis eu nunc. Fusce gravida, dui non dapibus pulvinar, nisi sem gravida purus, nec venenatis neque dolor molestie est. Aliquam sed magna justo. Praesent facilisis arcu at nibh posuere, a sagittis est blandit'
+	)
+
+User.create(
+	username: "test2" , 
+	email: "test2@test.com",
+	age: 44 ,
+	gender: 'password', 
+	password: 'password',
+	password_confirmation: 'password', 
+	bio: 'Auto-generated user. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar luctus diam, sit amet pulvinar lacus tincidunt vitae. Duis porttitor velit leo, id interdum eros aliquam vel. Aenean finibus imperdiet blandit. Praesent condimentum ante nibh, in vestibulum mauris dapibus sit amet. Mauris tincidunt non tortor eu pulvinar. Morbi lacinia lobortis nibh sed accumsan. Suspendisse ac metus tincidunt metus congue luctus quis eu nunc. Fusce gravida, dui non dapibus pulvinar, nisi sem gravida purus, nec venenatis neque dolor molestie est. Aliquam sed magna justo. Praesent facilisis arcu at nibh posuere, a sagittis est blandit'
+	)
+
+User.create(
+	username: "test3" , 
+	email: "test3@test.com",
+	age: 44 ,
+	gender: 'password', 
+	password: 'password',
+	password_confirmation: 'password', 
+	bio: 'Auto-generated user. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar luctus diam, sit amet pulvinar lacus tincidunt vitae. Duis porttitor velit leo, id interdum eros aliquam vel. Aenean finibus imperdiet blandit. Praesent condimentum ante nibh, in vestibulum mauris dapibus sit amet. Mauris tincidunt non tortor eu pulvinar. Morbi lacinia lobortis nibh sed accumsan. Suspendisse ac metus tincidunt metus congue luctus quis eu nunc. Fusce gravida, dui non dapibus pulvinar, nisi sem gravida purus, nec venenatis neque dolor molestie est. Aliquam sed magna justo. Praesent facilisis arcu at nibh posuere, a sagittis est blandit'
+	)
+
+User.create(
+	username: "test4" , 
+	email: "test4@test.com",
+	age: 44 ,
+	gender: 'password', 
+	password: 'password',
+	password_confirmation: 'password', 
+	bio: 'Auto-generated user. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar luctus diam, sit amet pulvinar lacus tincidunt vitae. Duis porttitor velit leo, id interdum eros aliquam vel. Aenean finibus imperdiet blandit. Praesent condimentum ante nibh, in vestibulum mauris dapibus sit amet. Mauris tincidunt non tortor eu pulvinar. Morbi lacinia lobortis nibh sed accumsan. Suspendisse ac metus tincidunt metus congue luctus quis eu nunc. Fusce gravida, dui non dapibus pulvinar, nisi sem gravida purus, nec venenatis neque dolor molestie est. Aliquam sed magna justo. Praesent facilisis arcu at nibh posuere, a sagittis est blandit'
+	)
+
+User.create(
+	username: "test5" , 
+	email: "test5@test.com",
+	age: 44 ,
+	gender: 'password', 
+	password: 'password',
+	password_confirmation: 'password', 
+	bio: 'Auto-generated user. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar luctus diam, sit amet pulvinar lacus tincidunt vitae. Duis porttitor velit leo, id interdum eros aliquam vel. Aenean finibus imperdiet blandit. Praesent condimentum ante nibh, in vestibulum mauris dapibus sit amet. Mauris tincidunt non tortor eu pulvinar. Morbi lacinia lobortis nibh sed accumsan. Suspendisse ac metus tincidunt metus congue luctus quis eu nunc. Fusce gravida, dui non dapibus pulvinar, nisi sem gravida purus, nec venenatis neque dolor molestie est. Aliquam sed magna justo. Praesent facilisis arcu at nibh posuere, a sagittis est blandit'
+	)
+
+p "Test Users Created"
