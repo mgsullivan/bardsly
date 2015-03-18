346cb5275135dea9eb0474d0b95a8b24724eaafd
Matthew Sullivan <mgsullivan.musc@gmail.com>
2015-03-17 20:16:40 -0400

    added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def show
 
   end
+  def index
+  	@users = User.all
+  end
   private
   def set_user
   	@user = User.find_by(username: params[:id])
